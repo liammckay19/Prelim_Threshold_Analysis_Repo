7c93d452632b9b32b2aca475663cf8ded566f90f
Liam McKay <ltmckay@ucsc.edu>
2018-01-15 14:25:44 -0800

    using smaller piece of data, creating df1 excluding 0.0 from sample
    smaller piece of data because my computer is running slow on the data

--- outlierResults_sample_column_95thpercentile.R
+++ outlierResults_sample_column_95thpercentile.R
@@ -4,13 +4,23 @@ library(tidyverse)
 
 
 setwd("~/Documents/UCSC/Junior/Treehouse/Prelim_Threshold_Analysis_Repo/comp4.3_tert8.ckcc.outlier_results")
-up_outlier_files=list.files(, "outlier_results_")
+# up_outlier_files=list.files(, "outlier_results_")
 
-outlierResults<-lapply(up_outlier_files, function(x) {
+# outlierResults<-lapply(up_outlier_files, function(x) {
+# 	read_tsv(x, col_types=cols()) %>% 
+# 	add_column(sampleID=gsub("outlier_results_", "", x))
+# 	}) 	%>% 
+# 	bind_rows()
+
+up_outlier_files_small = list.files(, "outlier_results_TH01_0053_S01") 
+# so my computer can handle the data set faster for initial testing
+outlierResultsSmall <- lapply(up_outlier_files, function(x) {
 	read_tsv(x, col_types=cols()) %>% 
-	add_column(sampleID=gsub("outlier_results_", "", x))
+	add_column(sampleID=gsub("outlier_results_TH01_0053_S01", "", x))
 	}) 	%>% 
 	bind_rows()
 
-sampleColumn = outlierResults$sampleID
-quantile(sampleColumn, c(.95))
+df1 <- outlierResultsSmall %>% 
+    select(Gene, sample) %>%
+    filter(sample > 0.0) %>% #filter out outliers that are 0.0
+
